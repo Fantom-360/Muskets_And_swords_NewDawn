008c73242d6c7e5046d3c1575a2f559416143241
Fantom-360 <sammlynek7@gmail.com>
2025-09-10 07:41:19 +0100

    minor update: renaming stuff,thinking about refactoring code and making it in a game engine don't know for now looking for a game engine.

---
+++ src/hex_props/hex_items/item_calls.py
@@ -0,0 +1,28 @@
+import os
+
+def call_img(season:str, land_type:str):
+    """
+    Parameters:
+    season (str): "1"- spring "2"-Winter,
+    land_type (str): "1"-plains "2"-forest "3"-hill
+
+    """
+    image_folder = image_folder = os.path.join(os.getcwd(),'source', 'hex_props', 'hex_items', 'images')
+    terrain = { 
+        "1" : {
+            "1": os.path.join(image_folder, r'plains_spr.png'),
+            "2": os.path.join(image_folder, r'forest_spr.png'),
+            "3": os.path.join(image_folder, r'hills_spring.png'),
+            "4": os.path.join(image_folder, r'water.png')
+        },
+        "2" : {
+            "1": os.path.join(image_folder, r"plains_win.png"),
+            "2": os.path.join(image_folder, r"forest_win.png"),
+            "3": os.path.join(image_folder, r"hills_winter.png"),
+            "3": os.path.join(image_folder, r"Ice.png"),
+        }
+    }
+    
+    return terrain[season][land_type]
+
+
