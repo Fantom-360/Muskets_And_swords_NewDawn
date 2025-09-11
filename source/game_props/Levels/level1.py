a6a1e665b039163d6143ed8fdf05c77ec7d19b14
Fantom-360 <sammlynek7@gmail.com>
2025-09-11 19:48:07 +0100

    okay please work git

--- source/game_props/Levels/level1.py
+++
@@ -1,9 +0,0 @@
-from hex_props.Hexagons import Hex_node
-
-
-level= {
-    (0, 0) : Hex_node(0, 0, ("1", "1")),
-    (1, 0) : Hex_node(1, 0, ("1", "2")),
-    (0, 1) : Hex_node(0, 1, ("1", "2")),
-    (1, 1) : Hex_node(1, 1, ("1", "4"))
-}
